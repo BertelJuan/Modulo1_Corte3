4964f028a6defb209a8bcfd52dd7308f3cb4f1a9
BertelJuan <berteljuan77@gmail.com>
2025-11-25 10:58:06 -0500

    Segundo o Tercer Deploy

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -17,7 +17,7 @@ resource "aws_api_gateway_method" "post_shorten" {
 
 resource "aws_api_gateway_integration" "shorten_integration" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.shorten.shorten.id
+  resource_id = aws_api_gateway_resource.shorten.id
   http_method = aws_api_gateway_method.post_shorten.http_method
   integration_http_method = "POST"
   type = "AWS_PROXY"
