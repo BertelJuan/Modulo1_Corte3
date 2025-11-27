a5387c08fc4f8267bc8732674fedd3acfe4aff5a
BertelJuan <berteljuan77@gmail.com>
2025-11-27 13:54:13 -0500

    Sigan Viendo - Solucionando error

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -41,7 +41,7 @@ resource "aws_api_gateway_deployment" "deploy" {
   rest_api_id = aws_api_gateway_rest_api.api.id
 
   triggers = {
-    redeploy = timestamp()
+    redeploy = sha1(jsonencode(aws_api_gateway_rest_api.api))
   }
 }
 
