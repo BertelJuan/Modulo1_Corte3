16938094ff60950445d1c070ed40ce2b348b564f
BertelJuan <berteljuan77@gmail.com>
2025-11-28 11:26:06 -0500

    Sigan Viendo - Todavia

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,3 +10,16 @@ output "lambda_arn" {
 output "table_name" {
   value = var.url_table
 }
+
+# outputs.tf del Módulo 1
+output "api_id" {
+  value = aws_api_gateway_rest_api.api.id
+}
+
+output "root_id" {
+  value = aws_api_gateway_rest_api.api.root_resource_id
+}
+
+output "execution_arn" {
+  value = aws_api_gateway_rest_api.api.execution_arn
+}
