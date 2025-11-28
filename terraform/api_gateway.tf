b69e9783a99e27fbc090b62609b7155f564b8db3
BertelJuan <berteljuan77@gmail.com>
2025-11-28 11:12:23 -0500

    Sigan Viendo - Espero que funcione

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -33,10 +33,10 @@ resource "aws_lambda_permission" "api_permission" {
 }
 
 resource "aws_api_gateway_deployment" "deploy" {
-  depends_on = [
-    aws_api_gateway_integration.shorten_integration,
-    aws_api_gateway_integration.options_shorten_integration
-  ]
+  //depends_on = [
+    //aws_api_gateway_integration.shorten_integration,
+    //aws_api_gateway_integration.options_shorten_integration
+  //]
 
   rest_api_id = aws_api_gateway_rest_api.api.id
   triggers = {
